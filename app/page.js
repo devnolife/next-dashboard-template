458a2271f846c0486f5220dd2117ee83492fbf9c
ilham1421 <105841105822@student.unismuh.ac.id>
2024-07-25 14:46:37 +0800

    selesai chapter 2

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-6">
-      <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
+      <div className="flex items-end h-20 p-4 bg-red-500 rounded-lg shrink-0 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <a href="https://nextjs.org/learn/" className="text-red-500">
               Next.js Learn Course
             </a>
             , brought to you by devnolife - laboratorium-if-unismuh.
@@ -27,6 +27,8 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
+
+        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       </div>
     </main>
   );
